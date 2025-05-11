7146c48d6970c8e3a851e32c77b66a82c55c0cc2
jorieltejada28 <jorieltejada84@gmail.com>
2025-05-11 14:17:31 +0800

    commit

--- obj/Debug/net9.0/webApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/webApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0966c3175497c5a1c822d4c15ce1a33c39034be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdece78e192e7f9b089c88388c6c86f25273a418")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/drop-down.js
@@ -0,0 +1,4 @@
+    function toggleDropdown(id) {
+        const dropdown = document.getElementById(id);
+        dropdown.classList.toggle('hidden');
+    }
